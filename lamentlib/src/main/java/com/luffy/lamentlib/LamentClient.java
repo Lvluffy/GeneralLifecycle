31a105476d2aec0dd5c8646275ffb55e41b6044f
lvlufei <lvlufei@aoji.cn>
2020-04-10 18:36:23 +0800

    去除androidx

--- lamentlib/src/main/java/com/luffy/lamentlib/LamentClient.java
+++ lamentlib/src/main/java/com/luffy/lamentlib/LamentClient.java
@@ -6,10 +6,9 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 /**
  * Created by lvlufei on 2020-04-10
