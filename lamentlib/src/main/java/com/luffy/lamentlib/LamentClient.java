69e59b6ff2ad024954cc68eb992a4385c6b27dab
lvlufei <lvlufei@aoji.cn>
2020-04-10 18:23:14 +0800

    first time commit

---
+++ lamentlib/src/main/java/com/luffy/lamentlib/LamentClient.java
@@ -0,0 +1,64 @@
+package com.luffy.lamentlib;
+
+import android.app.Activity;
+import android.app.Application;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * Created by lvlufei on 2020-04-10
+ *
+ * @name 哀悼
+ * @desc
+ */
+public class LamentClient {
+
+    public static void install(Application application) {
+        application.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+                Paint paint = new Paint();
+                ColorMatrix colorMatrix = new ColorMatrix();
+                colorMatrix.setSaturation(0);
+                paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
+                activity.getWindow().getDecorView().setLayerType(View.LAYER_TYPE_HARDWARE, paint);
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+        });
+    }
+}
