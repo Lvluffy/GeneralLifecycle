cf5198fe00b2796864c2ead979b24b1b2bd8698c
lvlufei <lvlufei@aoji.cn>
2020-04-10 18:28:31 +0800

    去除androidx

--- app/src/main/java/com/luffy/generallifecycle/MainActivity.java
+++ app/src/main/java/com/luffy/generallifecycle/MainActivity.java
@@ -1,8 +1,7 @@
 package com.luffy.generallifecycle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
